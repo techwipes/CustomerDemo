13a62957a1ea5e45c734e0e1a9ec787255c7ab50
Mitya <techwipes@gmail.com>
2020-10-20 15:22:18 +0500

    add logging by Slf4j

--- src/main/java/org/techwipes/customerdemo/service/CustomerServiceImpl.java
+++ src/main/java/org/techwipes/customerdemo/service/CustomerServiceImpl.java
@@ -8,29 +8,38 @@ import org.techwipes.customerdemo.repository.CustomerRepository;
 
 import java.util.List;
 
-@Service
+
+/**
+ * Implementations of {@link CustomerService} interface.
+ */
+
 @Slf4j
+@Service
 public class CustomerServiceImpl implements CustomerService{
     @Autowired
     CustomerRepository customerRepository;
 
     @Override
     public Customer getById(Long id) {
+        log.info("IN CustomerServiceImpl getById", id);
         return customerRepository.getOne(id);
     }
 
     @Override
     public void save(Customer customer) {
+        log.info("IN CustomerServiceImpl save", customer);
         customerRepository.save(customer);    }
 
     @Override
     public void delete(Long id) {
+        log.info("IN CustomerServiceImpl delete", id);
         customerRepository.deleteById(id);
 
     }
 
     @Override
     public List<Customer> getAll() {
+        log.info("IN CustomerServiceImpl findAll");
         return customerRepository.findAll();
     }
 }

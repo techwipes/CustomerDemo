d7a1755e803600dc04f797a53b28adf84030f767
Mitya <techwipes@gmail.com>
2020-10-20 16:04:36 +0500

    add entry point for app

---
+++ src/main/java/org/techwipes/customerdemo/CustomerDemoApplication.java
@@ -0,0 +1,13 @@
+package org.techwipes.customerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class CustomerDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerDemoApplication.class);
+    }
+}
